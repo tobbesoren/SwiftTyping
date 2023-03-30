4282bb47ddfa99d8239531861f9e33bc963cc97f
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-03-30 10:01:27 +0200

    addedBackButtons

--- SwiftTyping/ViewControllers/HiScoreTableViewController.swift
+++ SwiftTyping/ViewControllers/HiScoreTableViewController.swift
@@ -40,7 +40,7 @@ class HiScoreTableViewController: UITableViewController {
         return cell
     }
    
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
